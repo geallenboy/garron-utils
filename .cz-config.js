f1e4524ea393fb87e5b56ec6f3d71af8ee22f230
geallenboy <gejialun88@163.com>
2022-01-20 11:29:41 +0800

    feat: code

--- .cz-config.js
+++ .cz-config.js
@@ -1,28 +1,29 @@
 module.exports = {
   // 可选类型
   types:[
-    { value: '✨特性', name: '特性:    一个新的特性' },
-    { value: '🐛修复', name: '修复:    修复一个Bug' },
-    { value: '📝文档', name: '文档:    变更的只有文档' },
-    { value: '💄格式', name: '格式:    空格, 分号等格式修复' },
-    { value: '♻️重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-    { value: '⚡️性能', name: '性能:    提升性能' },
-    { value: '✅测试', name: '测试:    添加一个测试' },
-    { value: '🔧工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-    { value: '⏪回滚', name: '回滚:    代码回退' }
+    { value: 'feat',     name: 'feat:✨ Features | 新功能'},
+    { value: 'fix',      name: 'fix:🐛 Bug Fixes | Bug 修复'},
+    { value: 'docs',     name: 'docs:📚 Documentation | 文档'},
+    { value: 'style',    name: 'style:💎 Styles | 风格'},
+    { value: 'init',     name: 'init:🎉 Init | 初始化'},
+    { value: 'refactor', name: 'refactor:📦 Code Refactoring | 代码重构'},
+    { value: 'pref',     name: 'pref:🚀 Performance Improvements | 性能优化'},
+    { value: 'test',     name: 'test:✅ Tests | 测试'},
+    { value: 'chore',    name: 'chore:♻️ Chore | 构建/工程依赖/工具'},
+    { value: 'revert',   name: 'revert:⏪ Revert | 回退'},
+    { value: 'build',    name: 'build:🛠‍ Build System | 打包构建'},
+    { value: 'ci',       name: 'ci:👷 Continuous Integration | CI 配置'}
   ],
-
+  
   // 步骤
   messages: {
-    type: '请选择提交的类型；',
-    customScope: '请输入修改的范围（可选）',
-    subject: '请简要描述提交（必填）',
-    body: '请输入详细描述（可选）',
-    footer: '请选择要关闭的issue（可选）',
+    type: '请选择提交的类型:',
+    customScope: '请输入修改的范围(可选):',
+    subject: '请简要描述提交(必填):',
+    // body: '请输入详细描述(可选):',
+    // footer: '请选择要关闭的issue（可选）',
     confirmCommit: '确认要使用以上信息提交？（y/n）'
   },
-  allowCustomScopes: true,
-  allowBreakingChanges: ['特性', '修复'],
   // 跳过步骤
   skip: ['body', 'footer'],
   // 默认长度
